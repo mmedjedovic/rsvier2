cc28b1473e5115194dee3df078667ed40647b589
Marko Medjedovic <medjedo@gmail.com>
2018-05-10 16:55:12 +0200

    kleine correctie

--- Aplikassie2/src/main/java/com/rsvier/workshop/KlantFormulierController.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/KlantFormulierController.java
@@ -1,9 +1,11 @@
 package com.rsvier.workshop;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/klantformulier")
 public class KlantFormulierController {
 	
