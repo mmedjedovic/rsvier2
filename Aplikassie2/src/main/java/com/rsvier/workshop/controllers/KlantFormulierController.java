e2272dfc48b5285eacfb8d42b2d406b90754b2db
Marko Medjedovic <medjedo@gmail.com>
2018-05-11 17:48:04 +0200

    form crieeren opgelost

--- Aplikassie2/src/main/java/com/rsvier/workshop/controllers/KlantFormulierController.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/controllers/KlantFormulierController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rsvier.workshop.domein.Persoon;
+import com.rsvier.workshop.domein.Persoon.AccountSoort;
+
 @Controller
 @RequestMapping("/klantformulier")
 public class KlantFormulierController {
 	
 	@GetMapping
 	public String getFormulier(Model model) {
+		model.addAttribute("persoon", new Persoon(AccountSoort.KLANT));
 		return "klantformulier";
 	}
 	
