b9691c4d228238775f373ee9b351958d0e8ac9b8
Marko Medjedovic <medjedo@gmail.com>
2018-05-13 17:34:19 +0200

    kleine aanpassingen

--- Aplikassie2/src/main/java/com/rsvier/workshop/controllers/KlantOverzichtController.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/controllers/KlantOverzichtController.java
@@ -3,6 +3,7 @@ package com.rsvier.workshop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
