79fae2d05825b796cd4bed3dd5ec16a79979a999
FilipLaptop <filip.vedder@gmail.com>
2018-05-17 16:57:29 +0200

    Ongebruikte import verwijderd

--- Aplikassie2/src/main/java/com/rsvier/workshop/controllers/BestelRegelController.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/controllers/BestelRegelController.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
