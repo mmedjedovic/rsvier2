55025d08ef79b24d6f4dfd2fa40ce3f02998536b
FilipLaptop <filip.vedder@gmail.com>
2018-05-17 16:57:43 +0200

    ongebruikte methode verwijderd

--- Aplikassie2/src/main/java/com/rsvier/workshop/dao/BestellingRepository.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/dao/BestellingRepository.java
@@ -4,13 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rsvier.workshop.domein.Bestelling;
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface BestellingRepository extends CrudRepository<Bestelling, Long>{
-    @Query(value = "SELECT * FROM bestelling b WHERE b.status = 'OPEN' OR b.status = 'VERZONDEN'",
-            nativeQuery=true)
-    public List<Bestelling> findActief();
     public Bestelling findByFactuurnummer(Long factuurnummer);
 }
