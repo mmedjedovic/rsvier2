2a15d7923cf1bb7c09d5153be642a86185c1aec2
FilipLaptop <filip.vedder@gmail.com>
2018-05-18 11:43:59 +0200

    Wat dingen reformatted

--- Aplikassie2/src/main/java/com/rsvier/workshop/controllers/ArtikelController.java
+++ Aplikassie2/src/main/java/com/rsvier/workshop/controllers/ArtikelController.java
@@ -16,84 +16,80 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="/artikel")
+@RequestMapping(value = "/artikel")
 public class ArtikelController {
-	
-        @Autowired
-	private ArtikelRepository artikelRepository;
 
-        @ModelAttribute("artikelen")
-        public List<Artikel> artikelen() {
-            List<Artikel> artikelen = artikelRepository.findActief();
-            return artikelen;
-        }
-	
-        @GetMapping
-	public String getOverizicht() {
-            return "artikel";
-	}
-	
-	@GetMapping(value="/add")
-	public String artikelToevoegformulier(@ModelAttribute Artikel artikel) {
-            return "artikelformulier";
-        }
-        
-        @PostMapping(value="/add")
-        public ModelAndView artikelToegevoegen (@Valid Artikel artikel, BindingResult bindingResult) {
-            
-            if (bindingResult.hasErrors()) {
-                ModelAndView modelAndView = new ModelAndView("artikelformulier");
-                return modelAndView;
-            }
-            
-            else {
-                artikel.setArtikelStatus(Artikel.ArtikelStatus.ACTIEF);
-                artikelRepository.save(artikel);
-                ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
-                return modelAndView;
-            }    
-        }
-        
-        @GetMapping(value="/edit")
-	public ModelAndView artikelWijzigen(@RequestParam(value="id", required=true) Long id) {
-		
-            Optional artikelOptional = artikelRepository.findById(id);
-            Artikel artikel = (Artikel) artikelOptional.get();
+    @Autowired
+    private ArtikelRepository artikelRepository;
 
-            if(artikel.getArtikelStatus() == Artikel.ArtikelStatus.INACTIEF) {
-                ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
-                return modelAndView;
-            }
+    @ModelAttribute("artikelen")
+    public List<Artikel> artikelen() {
+        List<Artikel> artikelen = artikelRepository.findActief();
+        return artikelen;
+    }
 
-            else {
-                ModelAndView modelAndView = new ModelAndView("artikelformulier");
-                modelAndView.addObject("artikel",artikel);
-                return modelAndView;
-            }
-	}
-	
-	@PostMapping(value="/edit")
-	public ModelAndView artikelGewijzigd(Artikel artikel) {
+    @GetMapping
+    public String getOverizicht() {
+        return "artikel";
+    }
+
+    @GetMapping(value = "/add")
+    public String artikelToevoegformulier(@ModelAttribute Artikel artikel) {
+        return "artikelformulier";
+    }
+
+    @PostMapping(value = "/add")
+    public ModelAndView artikelToevoegen(@Valid Artikel artikel, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            ModelAndView modelAndView = new ModelAndView("artikelformulier");
+            return modelAndView;
+        } else {
             artikel.setArtikelStatus(Artikel.ArtikelStatus.ACTIEF);
             artikelRepository.save(artikel);
             ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
             return modelAndView;
-	}
+        }
+    }
 
-	@GetMapping(value="/delete")
-	public ModelAndView verwijderBevestiging(@RequestParam(value="id", required=true) Long id) {
-            ModelAndView modelAndView = new ModelAndView("artikeldelete");
-            Optional artikelOptional = artikelRepository.findById(id);
-            Artikel artikel = (Artikel) artikelOptional.get();
-            modelAndView.addObject("artikel",artikel);
-            return modelAndView;
-	}
-        
-        @PostMapping(value="/delete")
-	public ModelAndView verwijderArtikelPagina(Artikel artikel) {
-            artikel.setArtikelStatus(Artikel.ArtikelStatus.INACTIEF);
-            artikelRepository.save(artikel);
+    @GetMapping(value = "/edit")
+    public ModelAndView artikelWijzigen(@RequestParam(value = "id", required = true) Long id) {
+
+        Optional artikelOptional = artikelRepository.findById(id);
+        Artikel artikel = (Artikel) artikelOptional.get();
+
+        if (artikel.getArtikelStatus() == Artikel.ArtikelStatus.INACTIEF) {
             ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
             return modelAndView;
-	}
+        } else {
+            ModelAndView modelAndView = new ModelAndView("artikelformulier");
+            modelAndView.addObject("artikel", artikel);
+            return modelAndView;
+        }
+    }
+
+    @PostMapping(value = "/edit")
+    public ModelAndView artikelGewijzigd(Artikel artikel) {
+        artikel.setArtikelStatus(Artikel.ArtikelStatus.ACTIEF);
+        artikelRepository.save(artikel);
+        ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/delete")
+    public ModelAndView verwijderBevestiging(@RequestParam(value = "id", required = true) Long id) {
+        ModelAndView modelAndView = new ModelAndView("artikeldelete");
+        Optional artikelOptional = artikelRepository.findById(id);
+        Artikel artikel = (Artikel) artikelOptional.get();
+        modelAndView.addObject("artikel", artikel);
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/delete")
+    public ModelAndView verwijderArtikel(Artikel artikel) {
+        artikel.setArtikelStatus(Artikel.ArtikelStatus.INACTIEF);
+        artikelRepository.save(artikel);
+        ModelAndView modelAndView = new ModelAndView("redirect:/artikel");
+        return modelAndView;
+    }
 }
