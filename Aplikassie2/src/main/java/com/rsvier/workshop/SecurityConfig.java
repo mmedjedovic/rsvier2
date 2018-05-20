105ac7a5f680984158d2823b2e9131d32ec4a685
FilipLaptop <filip.vedder@gmail.com>
2018-05-20 11:52:10 +0200

    Inloggen met in-memory authentication

---
+++ Aplikassie2/src/main/java/com/rsvier/workshop/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.rsvier.workshop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = passwordEncoder();
+        auth
+            .inMemoryAuthentication()
+                .withUser("filip")
+                    .password(encoder.encode("kaasboer"))
+                    .authorities("ROLE_USER")
+                .and()
+                .withUser("marko")
+                    .password(encoder.encode("melkboer"))
+                    .authorities("ROLE_USER");
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
